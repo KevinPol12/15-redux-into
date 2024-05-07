16e0ef86a949efe87c33804b62afde6628272483
KevinPol12 <kevinpolanco12@gmail.com>
2024-05-07 10:13:39 -0600

    Setting up our redux store, consuming state, and dispatching actions to redux

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux";
+
 function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
+  /*2.) We should keep as much of the computations within the useSelector, rather
+  than relying too much on tweeking the final state within the code.
+  This useSelector is basically like subscribing to the consumption of the state 
+  so if the state in the store changes, then this component will rerender*/
+  const customer = useSelector((store) => store.customer.fullName);
+
+  return <h2>👋 Welcome, {customer}</h2>;
 }
 
 export default Customer;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-/**This ./store import is a temporal way to just run the code on import for testing */
 import store from "./store";
-
-store.dispatch({ type: "account/deposit", payload: 500 });
-store.dispatch({
-  type: "customer/createCustomer",
-  payload: {
-    fullName: "Kevin",
-    nationalID: "0105",
-    createdAt: new Date().toISOString(),
-  },
-});
-console.log(store.getState());
-/*This code above is just for testing if the refactored code under this structure
-continues to work */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*1.) With this we have successfully connected our redux store with REACT */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );

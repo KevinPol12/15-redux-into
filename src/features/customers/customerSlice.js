ec6a6f17ed4f527d8a8d3261e7f63c813ce55934
KevinPol12 <kevinpolanco12@gmail.com>
2024-05-06 20:43:27 -0600

    Implementing a modern redux file structure with a feature oriented approach

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,42 @@
+/*Why customerSlice? - Well, in reference to the slice of the redux store 
+associated to the customer */
+const initialStateCustomer = { fullName: "", nationalID: "", createdAt: "" };
+
+export default function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return { ...state, fullName: action.payload };
+    default:
+      return state;
+  }
+}
+
+/*Customer - actionCreators */
+export function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date().toISOString(),
+    },
+  };
+}
+
+export function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
+
+// /*Customer dispatching */
+// store.dispatch(createCustomer("Marco Tulio", "0105199700081"));
+// console.log(store.getState());
+
+// store.dispatch(updateName("Marco Julio"));
+// console.log(store.getState());
--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 /**This ./store import is a temporal way to just run the code on import for testing */
-import "./store";
+import store from "./store";
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+store.dispatch({
+  type: "customer/createCustomer",
+  payload: {
+    fullName: "Kevin",
+    nationalID: "0105",
+    createdAt: new Date().toISOString(),
+  },
+});
+console.log(store.getState());
+/*This code above is just for testing if the refactored code under this structure
+continues to work */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
